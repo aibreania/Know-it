71c4cf86aa54852a8fd6bc379f38aa4b62a2b023
Danning Peng <danningpeng@gmail.com>
2017-01-08 04:37:30 -0800

    Update User.php

--- app/User.php
+++ app/User.php
@@ -1,72 +1,38 @@
 <?php
 
-namespace App;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
-use Illuminate\Database\Eloquent\Model;
-use Request;
-use Hash;
-
-class User extends Model
+class CreateTableQuestions extends Migration
 {
-    public function signup()
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
     {
-        $check = $this->has_username_and_password();
+        Schema::create('questions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title',64);
+            $table->text('desc')->nullable()->comment('description');
+            $table->unsignedInteger('user_id');
+            $table->unsignedInteger('admin_id');
+            $table->timestamps();
+            $table->string('status')->default('ok');
 
-        if(!$check)
-            return ['status' => 0, 'msg' => 'Username and password cannot be empty'];
-
-        $username = $check[0];
-        $password = $check[1];
-
-        $user_exists = $this
-            ->where('username', $username)
-            ->exists();
-        if($user_exists)
-            return ['status' => 0, 'msg' => 'Username already used'];
-
-        $hashed_password = Hash::make($password);
-
-        $user = $this;
-        $user->password = $hashed_password;
-        $user->username = $username;
-        if($user->save())
-            return ['status' => 1, 'id' => $user->id];
-        else
-            return ['status' => 0, 'msg' => 'DB insert failed'];
+            $table->foreign('user_id')->references('id')->on('users');
+        });
     }
 
-
-    public function has_username_and_password() {
-        $username = Request::get('username');
-        $password = Request::get('password');
-
-        if($username && $password)
-            return [$username, $password];
-        return false;
-    }
-
-    public function login() {
-        $check = $this->has_username_and_password();
-        if(!$check)
-            return ['status' => 0, 'msg' => 'Username and password cannot be empty'];
-        $username = $check[0];
-        $password = $check[1];
-
-        $user = $this->where('username', $username)->first();
-
-        if(!$user)
-            return ['status' => 0, 'msg' => 'User not exist'];
-
-        $hashed_password = $user->password;
-        if(!Hash::check($password, $hashed_password))
-            return ['status' => 0, 'msg' => 'Wrong password'];
-
-        session()->put('username', $user->username);
-        session()->put('user_id', $user->id);
-
-        dd(session()->all());
-
-        return ['status' => 1, 'id' => $user->id];
-
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('questions');
     }
 }
